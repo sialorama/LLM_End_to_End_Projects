d0dd1c25599f05298635317112557a12fbb57fd9
sialorama <sialorama@gmail.com>
2024-02-28 13:53:18 +0100

    Upload project files

---
+++ Uber_Writer/README.md
@@ -0,0 +1,2 @@
+# Uber Writer
+Vous pouvez désormais créer des blogs parfaits avec l'aide de l'IA, Uber Writer est votre nouveau compagnon rédactionnel.
---
+++ Uber_Writer/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+#  set app to wide mode
+st.set_page_config(layout="wide")
+
+# title of our app
+st.title('Uber Writer')
+
+# create a subheader
+st.subheader("Vous pouvez désormais créer des blogs parfaits avec l'aide de l'IA, Uber Writer est votre nouveau compagnon rédactionnel.")
+
