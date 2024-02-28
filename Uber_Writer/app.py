e23f9c5d1274fe585ecf4b3c353317bd6358075c
sialorama <sialorama@gmail.com>
2024-02-28 14:21:41 +0100

    app sidebar code update

--- Uber_Writer/app.py
+++ Uber_Writer/app.py
@@ -9,3 +9,11 @@ st.title('Uber Writer')
 # create a subheader
 st.subheader("Vous pouvez désormais créer des blogs parfaits avec l'aide de l'IA, Uber Writer est votre nouveau compagnon rédactionnel.")
 
+# Sidebar for user input
+
+with st.sidebar:
+    st.title("Insérez les détails de votre blog")
+    st.subheader("Saisissez les détails du blog que vous voulez créer")
+    
+    # Blog title
+    blog_title = st.text_input("Titre du blog")
